edfb8c910222f5d51a75daa4c78eedabbcd6a1b5
Kili <kilimanjaro.a2@gmail.com>
2019-11-23 22:25:35 +0900

    Refactored codes

--- Assets/Plugins/FilledSprite/Samples/SampleFillAmountController.cs
+++ Assets/Plugins/FilledSprite/Samples/SampleFillAmountController.cs
@@ -4,64 +4,65 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public enum ButtonType
-{
-    Bottom,
-    Right,
-    Top,
-    Left
-}
-
-public class SampleFillAmountController : MonoBehaviour {
-    [SerializeField] private GameObject MyImageObject;
-    [SerializeField] private GameObject MySpriteObject;
-    private Image MyImage;
-    private Material MySpriteMaterial;
-    [SerializeField] private Slider MySlider;
-    [SerializeField] private Toggle MyToggle;
-    [SerializeField] private Button[] MyButtons;
-    [SerializeField] private Text OriginLabel;
-
-    void Start () {
-        MyImage = MyImageObject.GetComponent<Image>();
-        MySpriteMaterial = MySpriteObject.GetComponent<SpriteRenderer>().material;
+namespace KiliWare.FilledSprite.Sample {
+    public enum ButtonType
+    {
+        Bottom,
+        Right,
+        Top,
+        Left
     }
 
-    public void OnButtonChanged(int type){
-        MyImage.fillOrigin = type;
+    public class SampleFillAmountController : MonoBehaviour 
+    {
+        [SerializeField] private Image myImage;
+        [SerializeField] private SpriteRenderer mySpriteRenderer;
+        [SerializeField] private Slider fillAmountSlider;
+        [SerializeField] private Toggle clockwiseToggle;
+        [SerializeField] private Text fillOriginLabel;
+        private Material mySpriteMaterial;
 
-        float originX = 0;
-        float originY = 0;
-        switch((ButtonType)type){
-            case ButtonType.Bottom:
-                originY = -1;
-                break;
-            case ButtonType.Right:
-                originX = 1;
-                break;
-            case ButtonType.Top:
-                originY = 1;
-                break;
-            case ButtonType.Left:
-                originX = -1;
-                break;
+        void Awake ()
+        {
+            mySpriteMaterial = mySpriteRenderer.material;
         }
-        MySpriteMaterial.SetFloat("_FillOriginX", originX);
-        MySpriteMaterial.SetFloat("_FillOriginY", originY);
-        OriginLabel.text = "FillOrigin: " + Enum.GetName(typeof(ButtonType), (ButtonType)type);
-    }
 
-    public void OnClockwiseModeChanged()
-    {
-        bool value = MyToggle.isOn;
-        MyImage.fillClockwise = value;
-        MySpriteMaterial.SetFloat("_Clockwise", value ? 1 : 0);
-    }
+        public void OnButtonChanged(int type)
+        {
+            myImage.fillOrigin = type;
 
-    public void OnSliderChanged()
-    {
-        float value = MySlider.value;
-        MyImage.fillAmount = value;
-        MySpriteMaterial.SetFloat("_FillAmount", value);
+            float originX = 0, originY = 0;
+            switch((ButtonType)type){
+                case ButtonType.Bottom:
+                    originY = -1;
+                    break;
+                case ButtonType.Right:
+                    originX = 1;
+                    break;
+                case ButtonType.Top:
+                    originY = 1;
+                    break;
+                case ButtonType.Left:
+                    originX = -1;
+                    break;
+            }
+            mySpriteMaterial.SetFloat("_FillOriginX", originX);
+            mySpriteMaterial.SetFloat("_FillOriginY", originY);
+            fillOriginLabel.text = "FillOrigin: " + Enum.GetName(typeof(ButtonType), (ButtonType)type);
+        }
+
+        public void OnClockwiseModeChanged()
+        {
+            bool isClockWise = clockwiseToggle.isOn;
+            myImage.fillClockwise = isClockWise;
+            mySpriteMaterial.SetFloat("_Clockwise", isClockWise ? 1 : 0);
+        }
+
+        public void OnSliderChanged()
+        {
+            float fillAmountValue = fillAmountSlider.value;
+            myImage.fillAmount = fillAmountValue;
+            mySpriteMaterial.SetFloat("_FillAmount", fillAmountValue);
+        }
     }
 }

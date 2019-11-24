f1cdaee4b463a798902a3b5dfd42d82dd90dc98c
Kili <kilimanjaro.a2@gmail.com>
2019-11-24 19:45:37 +0900

    Introduced the SpritePositionAdjuster

---
+++ Assets/Plugins/FilledSprite/Samples/SpritePositionAdjuster.cs
@@ -0,0 +1,60 @@
+﻿// Retrieved from:
+// https://github.com/Kilimanjaro-a2/SpritePositionAdjuster/blob/a0cc0a1f2a111faa2f0259394122fa4d80b24511/Assets/Plugins/SpritePositionAdjuster/SpritePositionAdjuster.cs
+
+using UnityEngine;
+
+namespace KiliWare
+{
+    public class SpritePositionAdjuster : MonoBehaviour
+    {
+        public float OriginalScreenHeight = 0f;
+        public bool KeepsPositionUpdated = true;
+        protected Vector2 startPosition;
+        protected Vector2 startScale;
+        protected int previousScreenWidth;
+        protected int previousScreenHeight;
+
+        protected void Awake()
+        {
+            startScale = transform.localScale;
+            startPosition = transform.position - Camera.main.transform.position;
+
+            #if UNITY_EDITOR
+                Debug.Log("Current screen height is: " + Screen.height + ". If you want to preserve current scale and position of this sprites, set OriginalScreenHeight property " + Screen.height + " and restart the scene to check whether it works correctly.");
+            #endif
+
+            previousScreenWidth = Screen.width;
+            previousScreenHeight = Screen.height;
+
+            adjustSpritePosition();
+        }
+
+        public void adjustSpritePosition()
+        {
+            float ajustedHeight = OriginalScreenHeight / Screen.height;
+
+            transform.localScale = new Vector2(startScale.x, startScale.y) * ajustedHeight;
+
+            Vector2 adjustedPosition = startPosition * ajustedHeight;
+            adjustedPosition.y += Camera.main.transform.position.y;
+            transform.position = adjustedPosition;
+        }
+
+        // When you checkes "KeepsPositionUpdated",
+        // the sprite position's change follows the change of window size. 
+        protected void Update()
+        {
+            if (KeepsPositionUpdated && isScreenSizeChanged())
+            {
+                adjustSpritePosition();
+                previousScreenWidth = Screen.width;
+                previousScreenHeight = Screen.height;
+            }
+        }
+
+        protected bool isScreenSizeChanged()
+        {
+            return previousScreenHeight != Screen.height || previousScreenWidth != Screen.width;
+        }
+    }
+}
